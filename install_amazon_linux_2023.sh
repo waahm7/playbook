da6d115145e71ce2daca61a420d55ac9bb6009d0
Waqar Ahmed Khan <waahm7@gmail.com>
2024-06-02 13:30:07 -0700

    auto sync

---
+++ install_amazon_linux_2023.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo yum install -y ansible git 
+git clone https://github.com/waahm7/playbook ~/playbook
+cd ~/playbook
+ansible-playbook --ask-vault-pass setup.yml
+cd ~
+sudo rm -rf ~/playbook
