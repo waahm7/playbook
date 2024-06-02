525b8fb57d9802fda060215c6e6f76d8f3ee0c07
Waqar Ahmed Khan <waahm7@gmail.com>
2024-06-01 17:31:56 -0700

    add all tasks

---
+++ install_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt update -y
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible git 
+git clone https://github.com/waahm7/playbook ~/playbook
+cd ~/playbook
+ansible-playbook --ask-vault-pass setup.yml
+cd ~
+sudo rm -rf ~/playbook
